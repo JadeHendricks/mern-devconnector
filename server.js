11a223a1634188c3b878e4ed0226ecd939064d3f
jade <hendricksjade@gmail.com>
2020-06-17 14:15:39 +0200

    intial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const dotenv = require('dotenv');
+dotenv.config();
+const connectDB = require('./db');
+
+const auth = require('./routes/api/auth');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const post = require('./routes/api/post');
+
+//Connect to DB
+connectDB();
+
+//Routes
+app.use('/api/auth', auth);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/post', post);
+
+
+app.listen(port, () => console.log(`Server running on port:${port}`))
