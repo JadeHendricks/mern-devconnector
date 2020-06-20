8d560f016326c8fde585f08da904488077e5a4a2
jade <hendricksjade@gmail.com>
2020-06-20 19:30:00 +0200

    adding in the protective route component

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,12 @@ import React, { Fragment, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import PrivateRoute from './components/routing/PrivateRoute';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
 
@@ -36,6 +38,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={ Register } />
               <Route exact path='/login' component={ Login } />
+              <PrivateRoute exact path='/dashboard' component={ Dashboard } />
             </Switch>
           </section>
         </Fragment>
